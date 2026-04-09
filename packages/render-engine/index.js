8561b10eff2c2305c2b2c83b80c7e4396739224b
Daniel <yuilxin.liu@gmail.com>
2026-04-09 21:15:13 +0800

    更新项目结构

--- packages/render-engine/index.js
+++
@@ -1,9 +0,0 @@
-﻿const path = require('path');
-
-function resolveEngineSrcDir() {
-    return process.env.LUMESYNC_ENGINE_DIR || path.join(__dirname, '../engine/src');
-}
-
-module.exports = {
-    resolveEngineSrcDir
-};
